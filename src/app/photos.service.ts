2512f896d97733cb8012648259bc3846166820c3
Gustavonvp <gustavonvp@gmail.com>
2021-12-22 16:16:33 -0300

    URL OK

--- src/app/photos.service.ts
+++ src/app/photos.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+
+interface UnsplashResponse {
+  urls: {
+    regular: string;
+  }
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,7 +16,7 @@ export class PhotosService {
   constructor(private http: HttpClient) { }
 
   getPhoto() {
-    return this.http.get('https://api.unsplash.com/photos/random', {
+    return this.http.get<UnsplashResponse>('https://api.unsplash.com/photos/random', {
       headers: {
         Authorization: 'Client-ID BQhPRNjdb5HOMUIuqG6dE1_kZepNZ7Cec5iYEfuxGE0'
         
